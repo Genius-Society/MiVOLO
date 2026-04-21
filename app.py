45c04729bb2beaaa227987aa5eafbf1cb14e965d
admin <admin@noreply.com>
2026-04-21 13:22:16 +0800

    upl code

---
+++ app.py
@@ -0,0 +1,134 @@
+import os
+import cv2
+import imghdr
+import shutil
+import warnings
+import numpy as np
+import gradio as gr
+from dataclasses import dataclass
+from mivolo.predictor import Predictor
+from utils import is_url, download_file, get_jpg_files, _L, MODEL_DIR, TMP_DIR
+
+
+@dataclass
+class Cfg:
+    detector_weights: str
+    checkpoint: str
+    device: str = "cpu"
+    with_persons: bool = True
+    disable_faces: bool = False
+    draw: bool = True
+
+
+class ValidImgDetector:
+    predictor = None
+
+    def __init__(self):
+        detector_path = f"{MODEL_DIR}/yolov8x_person_face.pt"
+        age_gender_path = f"{MODEL_DIR}/model_imdb_cross_person_4.22_99.46.pth.tar"
+        predictor_cfg = Cfg(detector_path, age_gender_path)
+        self.predictor = Predictor(predictor_cfg)
+
+    def _detect(
+        self,
+        image: np.ndarray,
+        score_threshold: float,
+        iou_threshold: float,
+        mode: str,
+        predictor: Predictor,
+    ) -> np.ndarray:
+        predictor.detector.detector_kwargs["conf"] = score_threshold
+        predictor.detector.detector_kwargs["iou"] = iou_threshold
+        if mode == "Use persons and faces":
+            use_persons = True
+            disable_faces = False
+
+        elif mode == "Use persons only":
+            use_persons = True
+            disable_faces = True
+
+        elif mode == "Use faces only":
+            use_persons = False
+            disable_faces = False
+
+        predictor.age_gender_model.meta.use_persons = use_persons
+        predictor.age_gender_model.meta.disable_faces = disable_faces
+        detected_objects, out_im = predictor.recognize(image)
+        has_child, has_female, has_male = False, False, False
+        if len(detected_objects.ages) > 0:
+            has_child = _L("是") if min(detected_objects.ages) < 18 else _L("否")
+            has_female = _L("是") if "female" in detected_objects.genders else _L("否")
+            has_male = _L("是") if "male" in detected_objects.genders else _L("否")
+
+        return out_im[:, :, ::-1], has_child, has_female, has_male
+
+    def valid_img(self, img_path):
+        image = cv2.imread(img_path)
+        return self._detect(image, 0.4, 0.7, "Use persons and faces", self.predictor)
+
+
+def infer(photo: str):
+    status = "Success"
+    result = child = female = male = None
+    try:
+        if is_url(photo):
+            if os.path.exists(TMP_DIR):
+                shutil.rmtree(TMP_DIR)
+
+            photo = download_file(photo, f"{TMP_DIR}/download.jpg")
+
+        detector = ValidImgDetector()
+        if not photo or not os.path.exists(photo) or imghdr.what(photo) == None:
+            raise ValueError("请正确输入图片")
+
+        result, child, female, male = detector.valid_img(photo)
+
+    except Exception as e:
+        status = f"{e}"
+
+    return status, result, child, female, male
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    with gr.Blocks() as iface:
+        gr.Markdown(_L("# 性别年龄检测器"))
+        with gr.Tab(_L("上传模式")):
+            gr.Interface(
+                fn=infer,
+                inputs=gr.Image(label=_L("上传照片"), type="filepath"),
+                outputs=[
+                    gr.Textbox(label=_L("状态栏"), buttons=["copy"]),
+                    gr.Image(
+                        label=_L("检测结果"),
+                        type="numpy",
+                        buttons=["download", "fullscreen"],
+                    ),
+                    gr.Textbox(label=_L("存在儿童")),
+                    gr.Textbox(label=_L("存在女性")),
+                    gr.Textbox(label=_L("存在男性")),
+                ],
+                examples=get_jpg_files(f"{MODEL_DIR}/examples"),
+                flagging_mode="never",
+                cache_examples=False,
+            )
+
+        with gr.Tab(_L("在线模式")):
+            gr.Interface(
+                fn=infer,
+                inputs=gr.Textbox(label=_L("网络图片链接"), buttons=["copy"]),
+                outputs=[
+                    gr.Textbox(label=_L("状态栏"), buttons=["copy"]),
+                    gr.Image(
+                        label=_L("检测结果"),
+                        type="numpy",
+                        buttons=["download", "fullscreen"],
+                    ),
+                    gr.Textbox(label=_L("存在儿童")),
+                    gr.Textbox(label=_L("存在女性")),
+                    gr.Textbox(label=_L("存在男性")),
+                ],
+                flagging_mode="never",
+            )
+
+    iface.launch(css="#gradio-share-link-button-0 { display: none; }")
---
+++ utils.py
@@ -0,0 +1,98 @@
+import os
+import requests
+from tqdm import tqdm
+from urllib.parse import urlparse
+
+TMP_DIR = "./__pycache__"
+
+EN_US = os.getenv("LANG") != "zh_CN.UTF-8"
+
+
+if EN_US:
+    import huggingface_hub
+
+    MODEL_DIR = huggingface_hub.snapshot_download(
+        "Genius-Society/MiVOLO",
+        cache_dir="./mivolo/__pycache__",
+    )
+
+else:
+    import modelscope
+
+    MODEL_DIR = modelscope.snapshot_download(
+        "Genius-Society/MiVOLO",
+        cache_dir="./mivolo/__pycache__",
+    )
+
+
+ZH2EN = {
+    "# 性别年龄检测器": "# Gender Age Detector",
+    "上传模式": "Uploading Mode",
+    "上传照片": "Upload Photo",
+    "检测结果": "Detection Result",
+    "存在儿童": "Has Child",
+    "状态栏": "Status",
+    "存在女性": "Has Female",
+    "存在男性": "Has Male",
+    "在线模式": "Online Mode",
+    "网络图片链接": "Online Picture URL",
+    "是": "Yes",
+    "否": "No",
+}
+
+
+def _L(zh_txt: str):
+    return ZH2EN[zh_txt] if EN_US else zh_txt
+
+
+def is_url(s: str):
+    try:
+        # 解析字符串
+        result = urlparse(s)
+        # 检查scheme（如http, https）和netloc（域名）
+        return all([result.scheme, result.netloc])
+
+    except:
+        # 如果解析过程中发生异常，则返回False
+        return False
+
+
+def download_file(url: str, save_path: str):
+    if os.path.exists(save_path):
+        print("目标已存在，无需下载")
+        return
+
+    create_dir(os.path.dirname(save_path))
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    # 使用 tqdm 创建一个进度条
+    progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
+    with open(save_path, "wb") as file:
+        for data in response.iter_content(chunk_size=1024):
+            file.write(data)
+            progress_bar.update(len(data))
+
+    progress_bar.close()
+    if total_size != 0 and progress_bar.n != total_size:
+        os.remove(save_path)
+        print("下载失败，重试中...")
+        download_file(url, save_path)
+
+    else:
+        print("下载完成")
+
+    return save_path
+
+
+def create_dir(dir_path: str):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+def get_jpg_files(folder_path: str):
+    all_files = os.listdir(folder_path)
+    return [
+        os.path.join(folder_path, file)
+        for file in all_files
+        if file.lower().endswith(".jpg")
+    ]
